a1ca75767c5acf55a2224f43d274d60f1f2e28f7
geneaw <gene.williams@radicalregard.com>
2025-09-16 16:12:11 -0600

    Milestone fix up for Jazzy with robot motors working and gazebo harmonic movement fixed. Sim movement lidar and camera all working.

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -135,7 +135,7 @@ def generate_launch_description():
         gazebo,
         spawn_entity,
         diff_drive_spawner,
-        #joint_broad_spawner,
+        joint_broad_spawner,
         ros_gz_bridge,
         ros_gz_image_bridge
     ])
